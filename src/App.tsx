850bbe5a6d25c5a1e5ced70295609b41e9823ac1
miglemr <migler019@gmail.com>
2024-05-07 18:45:08 +0300

    chore: :tada: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="min-h-screen flex">
+      <div className="w-2/3 bg-stone-700"></div>
+      <div className="w-1/3"></div>
+    </div>
+  );
+}
+
+export default App;
